42a9d9517e017ac33c310b1ae7aa5b6c74db8c9b
tiuphun <phuong.nt210692@sis.hust.edu.vn>
2024-01-23 15:07:17 +0700

    practice 23.1

--- practice/2019.2/ARTROAD/ar.cpp
+++ practice/2019.2/ARTROAD/ar.cpp
@@ -1,14 +1,53 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 #define N 5009
-int n;
-int k;
-pair<int, int> ref[N];
+int n, K;
+int l, r;                       // range of arts for each artist
+int nbArt[N];                   // nb of art each artist can paint
+int maxArt = 0;                 // max num of art can be painted
+priority_queue<int, vector<int>, greater<int> > artist;  // min heap  
+// Keep track of artists with most nb of art
+vector<int> artCount(N, 0);
 
 int main(int argc, char const *argv[])
 {
-    cin >> n >> k;
-    for (int i = 0; i < k; i++) cin >> ref[i].first >> ref[i].second;
-    
+    cin >> n >> K;
+    for (int i = 1; i <= K; i++) {
+        cin >> l >> r;
+        nbArt[i] = r - l + 1;
+        for (int j = l; j <= r; j++) artCount[j]++;
+        cout << nbArt[i] << endl;
+    }
+    for (int i = 1; i <= K; i++) {
+        if (artist.size() < K-2) {
+            artist.push(nbArt[i]);
+        }
+        else if (nbArt[i] > artist.top()) {
+            artist.pop();
+            artist.push(nbArt[i]);
+        }
+    }
+    while (!artist.empty()) {
+        maxArt += artist.top();
+        artist.pop();
+    }
+    cout << maxArt << endl;
     return 0;
 }
+/*
+4 3
+1 1 
+2 2
+3 4
+
+2
+
+4 4
+1 1 
+2 2
+2 3
+3 4
+
+3   // does not handle same painting by multiple artist yet
+*/
--- practice/2019.2/PHOTO/photo.cpp
+++ practice/2019.2/PHOTO/photo.cpp
@@ -3,10 +3,28 @@ using namespace std;
 #define N 1009
 int n;
 int a[N];
+int totalPhoto = 0;
 int main(int argc, char const *argv[])
 {
     cin >> n;
     for (int i = 0; i < n; i++) cin >> a[i];
-    
+    sort(a, a+n);
+    int j, k;
+    for (int i = 0; i < n; ) {
+        j = i+1, k = i+2;
+        if (k < n && a[k] - a[i] <= 10) {
+            totalPhoto += 1;
+            i = k+1; 
+        }
+        else if (j < n && a[j] - a[i] <= 20) {
+            totalPhoto += 1;
+            i = j+1; 
+        }
+        else {
+            totalPhoto += 1;
+            i++;
+        }
+    }
+    cout << totalPhoto << endl;
     return 0;
 }
