77c00332513ac6592398aadf71ac54e00f4f1cdc
tiuphun <phuong.nt210692@sis.hust.edu.vn>
2024-01-29 21:23:14 +0700

    probably last backup

--- lab/lab4/gold/gold.cpp
+++ lab/lab4/gold/gold.cpp
@@ -1,18 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-#define N 100009
-int n, L1, L2, maxGold = 0;
-int a[N], dp[N][N];
-int main(int argc, char const *argv[])
-{
+const int N = 1e6+1;
+int a[N], S[N];
+int n, L1, L2, ans;
+void solve(){
+    deque<int> d;// luu tru chi so cac ung cu vien j tham gia vao viec xac dinh cac bai toan
+    // con S(i)
+    ans = 0;
+    for(int i = 1; i <= n; i++){
+        while(!d.empty() && d.front() < i - L2) d.pop_front();
+        int j = i - L1;
+        if(j >= 1){
+            while(!d.empty() && S[d.back()] < S[j]) d.pop_back();
+            d.push_back(j);
+        }
+        S[i] = a[i] + (d.empty() ? 0 : S[d.front()]);
+        ans = max(ans,S[i]);
+    }
+    cout << ans;
+}
+int main(){
     cin >> n >> L1 >> L2;
-    for (int i = 0; i < n; i++) cin >> a[i];
-
-    
-
-
-
-
-    cout << maxGold;
+    for(int i = 1; i <= n; i++) cin >> a[i];
+    solve();
     return 0;
 }
--- lab/lab5/ibus/ibus.cpp
+++ lab/lab5/ibus/ibus.cpp
@@ -1,15 +1,74 @@
 #include <iostream>
+#include <queue>
 using namespace std;
-#define N 5000
+#define N 5009
+#define INF 1000000000
 int n;              // nb of cities (1 <= n <= 500)
 int m;              // nb of edges (1 <= m <= 10000)
 int C[N];           // cost to get on the bus from point i (uniform for all j s.t. i -> j)
 int D[N];           // max nb. of cities to travel thru from i
+int w[N][N];        // weight of arc(u, v)
+int d[N];           // d[i] = distance source -> i during BFS
+bool f[N];          // true = shortest path s -> v found
+vector<int> A[N];   // A[v] list of adj cities of v
 
+void BFS(int i) {
+    queue<int> Q;
+    fill(d, d+N, -1);
+    d[i] = 0; Q.push(i);
+    while (!Q.empty()) {
+        int v = Q.front(); Q.pop();
+        w[i][v] = C[i]; 
+        for (int j = 0; j < A[v]. size(); j++) {
+            int u = A[v][j];
+            if (d[u] >= 0) continue;    // u visited
+            d[u] = d[v] + 1;
+            if (d[u] <= D[i]) Q.push(u);
+        }
+    }
+}
+void dijkstra(int s, int t) {
+    for (int v = 1; v <= n; v++) {
+        d[v] = w[s][v];
+        f[v] = false;
+    }
+    d[s] = 0; f[s] = true;
+    for (int i = 1; i <= n; i++) {
+        int u = -1; int minD = INF;
+        // select a node u have min d[u]
+        for (int v = 1; v <= n; v++) {
+            if (!f[v] && d[v] < minD) {
+                u = v;
+                minD = d[v];
+            }
+        }
+        f[u] = true;
+        if (u == t) break;          // result found
+        for (int v = 1; v <= n; v++) {
+            if (!f[v] && d[v] > d[u] + w[u][v]) 
+                d[v] = d[u] + w[u][v];
+        }
+    }
+    cout << d[t];
+}
+void buildGraph() {
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= n; j++)
+            w[i][j] = INF;
+    }
+    for (int i = 1; i <= n; i++) BFS(i);
+}
 int main(int argc, char const *argv[])
 {
     cin >> n >> m;
-    for (int i = 0; i < n; i++) cin >> C[i] >> D[i];
-    for (int i = 0; i < m; i++) 
+    for (int i = 1; i <= n; i++) cin >> C[i] >> D[i];
+    for (int i = 1; i <= m; i++) {
+        int u, v;
+        cin >> u >> v;
+        A[u].push_back(v);
+        A[v].push_back(u);
+    }
+    buildGraph();
+    dijkstra(1, n);
     return 0;
 }
